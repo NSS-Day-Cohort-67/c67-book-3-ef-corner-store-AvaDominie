909a4d5ddb0694ac6b452578dd6b74e5aa14a31c
Ava Dominie <avadominie@gmail.com>
2024-01-17 13:05:38 -0600

    Product endpoints

--- CornerStore/Program.cs
+++ CornerStore/Program.cs
@@ -41,6 +41,7 @@ app.UseHttpsRedirection();
 
 // cashier
 
+// adds a cashier
 app.MapPost("/api/cashiers", (CornerStoreDbContext db, Cashier cashier) =>
 {
     db.Cashiers.Add(cashier);
@@ -49,26 +50,20 @@ app.MapPost("/api/cashiers", (CornerStoreDbContext db, Cashier cashier) =>
 });
 
 
-// app.MapGet("/api/cashiers", (CornerStoreDbContext db) => 
-// {
-//     return db.Cashiers
-//     .Select(c => new CashierDTO
-//     {
-//         Id = c.Id,
-//         FirstName = c.FirstName,
-//         LastName = c.LastName
-//     }).ToList();
-// });
+// gets all cashiers
 app.MapGet("/api/cashiers", (CornerStoreDbContext db) =>
 {
     return db.Cashiers
+    // allows access to orders
     .Include(c => c.Orders)
+    // allows access to order products
         .ThenInclude(o => o.OrderProducts)
     .Select(c => new CashierDTO
     {
         Id = c.Id,
         FirstName = c.FirstName,
         LastName = c.LastName,
+        // access orders of CashierId
         Orders = c.Orders.Select(o => new OrderDTO
         {
             Id = o.Id,
@@ -79,6 +74,7 @@ app.MapGet("/api/cashiers", (CornerStoreDbContext db) =>
                 FirstName = c.FirstName,
                 LastName = c.LastName
             },
+            // access orderProducts of OrderId
             OrderProducts = o.OrderProducts.Select(op => new OrderProductDTO
             {
                 Id = op.Id,
@@ -90,6 +86,7 @@ app.MapGet("/api/cashiers", (CornerStoreDbContext db) =>
                     Price = op.Product.Price,
                     Brand = op.Product.Brand,
                     CategoryId = op.Product.CategoryId,
+                    // access Category of CategoryId
                     Category = new CategoryDTO
                     {
                         Id = op.Product.Category.Id,
@@ -99,7 +96,6 @@ app.MapGet("/api/cashiers", (CornerStoreDbContext db) =>
                 OrderId = op.OrderId,
                 Quantity = op.Quantity
             }).ToList(),
-
         }).ToList()
     }).ToList();
 });
@@ -110,9 +106,79 @@ app.MapGet("/api/cashiers", (CornerStoreDbContext db) =>
 // product
 
 
+// get all products
+// app.MapGet("/api/products", (CornerStoreDbContext db) =>
+// {
+//     return db.Products
+//     // allows access to categories
+//     .Include(c => c.Category)
+//     .Select(p => new ProductDTO
+//     {
+//         Id = p.Id,
+//         ProductName = p.ProductName,
+//         Price = p.Price,
+//         Brand = p.Brand,
+//         CategoryId = p.CategoryId,
+//         Category = new CategoryDTO
+//         {
+//             Id = p.Category.Id,
+//             CategoryName = p.Category.CategoryName
+//         }
+//     }).ToList();
+// });
+app.MapGet("/api/products", (CornerStoreDbContext db, string search) =>
+{
+    string lowerSearch = search.ToLower();
+
+    return db.Products
+    .Include(p => p.Category)
+    .Where(p => p.ProductName.ToLower().Contains(lowerSearch) ||
+                p.Category.CategoryName.ToLower().Contains(lowerSearch))
+    .Select(p => new ProductDTO
+    {
+        Id = p.Id,
+        ProductName = p.ProductName,
+        Price = p.Price,
+        Brand = p.Brand,
+        CategoryId = p.CategoryId,
+        Category = new CategoryDTO
+        {
+            Id = p.Category.Id,
+            CategoryName = p.Category.CategoryName
+        }
+    }).ToList();
+});
 
 
 
+
+// add a product
+app.MapPost("/api/products", (CornerStoreDbContext db, Product product) =>
+{
+    db.Products.Add(product);
+    db.SaveChanges();
+    return Results.Created($"/api/products/{product.Id}", product);
+});
+
+
+// update a product
+app.MapPut("/api/products/{id}", (CornerStoreDbContext db, int id, ProductDTO product) =>
+{
+    Product productToUpdate = db.Products.SingleOrDefault(product => product.Id == id);
+    if (productToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+
+    productToUpdate.ProductName = product.ProductName;
+    productToUpdate.Price = product.Price;
+    productToUpdate.Brand = product.Brand;
+    productToUpdate.CategoryId = product.CategoryId;
+
+
+    db.SaveChanges();
+    return Results.NoContent();
+});
 
 
 // order
